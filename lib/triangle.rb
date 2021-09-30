09b125909200e4c4cf654e6fc853a15a29eeecd0
Branden <brandenehman45@gmail.com>
2021-09-30 12:37:57 -0600

    Completed Lab

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,27 @@
 class Triangle
-  # write code here
+
+  attr_accessor :all_sides
+
+  @all_sides = []
+  
+  def initialize(a, b, c)
+    @all_sides = [a, b, c]
+    @all_sides.sort!
+  end
+
+  def kind
+    if @all_sides.any?{ |side| side <= 0 } || ( @all_sides[0] + @all_sides[1] <= @all_sides[2] )
+      raise TriangleError
+    elsif @all_sides.uniq.length == 1
+      :equilateral
+    elsif @all_sides.uniq.length == 2
+      :isosceles
+    else 
+      :scalene
+    end
+  end
+
+  class TriangleError < StandardError
+
+  end
 end
